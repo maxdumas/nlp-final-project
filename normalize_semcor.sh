e8b7379a1a6aa1817129fe3d79b5fb8ffffb4d77
Max Dumas <maltor124@gmail.com>
2015-12-14 23:35:09 -0500

    Add message instructing to download semcor in normalize_semcor.sh

--- normalize_semcor.sh
+++ normalize_semcor.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+if [ ! -d 'semcor3.0' ]; then
+    echo 'No semcor3.0 install found! Please ensure that semcor3.0 is available in this directory as the directory "semcor3.0".'
+    echo 'You can download the latest semcor3.0 distribution at http://web.eecs.umich.edu/~mihalcea/downloads.html#semcor'
+    exit 2
+fi
+
 mkdir data_train
 
 echo 'Normalizing semcor XML files and outputting to data_train...'
@@ -12,11 +18,3 @@ echo 'Tranforming semcor XML files into sense-annotated key data and plain test 
 python3 generate_key_test_semcor.py
 
 echo 'Done.'
-
-# for filename in ./semcor3.0/brown2/tagfiles/*; do
-#     cat "$filename" | tidy -xml -i - > "./training_data/$(basename "$filename").xml"
-# done
-
-# for filename in ./semcor3.0/brownv/tagfiles/*; do
-#     cat "$filename" | tidy -xml -i - > "./training_data/$(basename "$filename").xml"
-# done
