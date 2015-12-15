c2162f778fbcb8b8bd085b4a4c577d18524f968d
Max Dumas <maltor124@gmail.com>
2015-12-15 00:12:54 -0500

    Allow normalize_semcor to automatically download semcor3.0 if it is not found locally

--- normalize_semcor.sh
+++ normalize_semcor.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 if [ ! -d 'semcor3.0' ]; then
-    echo 'No semcor3.0 install found! Please ensure that semcor3.0 is available in this directory as the directory "semcor3.0".'
-    echo 'You can download the latest semcor3.0 distribution at http://web.eecs.umich.edu/~mihalcea/downloads.html#semcor'
-    exit 2
+    echo 'No semcor install found! Attempting to download semcor3.0...'
+    if ! curl -O http://web.eecs.umich.edu/~mihalcea/downloads/semcor/semcor3.0.tar.gz; then
+        echo 'Semcor3.0 could not be automatically downloaded! Please ensure that semcor3.0 is available in this directory as the directory "semcor3.0".'
+        echo 'You can download the latest semcor3.0 distribution at http://web.eecs.umich.edu/~mihalcea/downloads.html#semcor'
+        exit 2
+    elif ! tar -xf semcor3.0.tar.gz; then
+        exit 3
+    else
+        rm -f semcor3.0.tar.gz
+    fi
 fi
 
 mkdir data_train
