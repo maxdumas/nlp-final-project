c388af1ea54b3b3d268c05e4d2d1c4841d61abc9
Max Dumas <maltor124@gmail.com>
2015-12-14 23:31:55 -0500

    Finalize features

--- features.py
+++ features.py
@@ -1,20 +1,28 @@
+from collections import defaultdict
+from nltk.corpus import wordnet as wn
+
 def extract_features(word_form, i, sentence):
     n = len(sentence)
-    features = {}
+    features = defaultdict(lambda: None)
 
     features['word'] = word_form['word']
-    features['pos'] = word_form['pos']
-    features['lemma'] = word_form['lemma']
+    features['q:pos'] = word_form['pos'][:2]
+    features['q:lemma'] = word_form['lemma']
 
     # Extract collocation features from k previous and k following forms
-    for k in range(-5, 5):
+    for k in range(-1, 2):
         if k == 0: continue
 
         if i + k < -1 or i + k >= n:
-            features['pos{:+}'.format(k)] = None
+            features['c:pos{:+}'.format(k)] = None
         elif i + k == -1:
-            features['pos{:+}'.format(k)] = '.'
-        elif 'pos' in sentence[i + k]:
-            features['pos{:+}'.format(k)] = sentence[i + k]['pos']
+            features['c:pos{:+}'.format(k)] = '.'
+        else:
+            features['c:pos{:+}'.format(k)] = sentence[i + k]['pos'][:2]
 
+    # Extract bag-of-words features from k previous and k following forms
+    for k in range(-2, 0):
+        if k == 0: continue
+
+        features['b:near_sentence_start'] = i + k <= -1
     return features
