5354586708969a87410bd0db9fd9af647e79548e
Max Dumas <maltor124@gmail.com>
2015-12-15 00:14:28 -0500

    Do semcor existence check for all run scripts

--- run-all-baseline.sh
+++ run-all-baseline.sh
@@ -2,6 +2,13 @@ if [ ! -d 'data_train_omit' ]; then
     mkdir data_train_omit
 fi
 
+if [ ! -d 'data_test' ]; then
+    echo 'Test data not found! Normalizing semcor...'
+    if ! ./normalize_semcor.sh; then
+        exit 2
+    fi
+fi
+
 for test_file in ./data_test/*; do
     file_name="$(basename ${test_file%.*})"
     mv "data_train/$file_name.xml" data_train_omit
